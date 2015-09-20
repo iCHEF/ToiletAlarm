ba8faee9c38376babbdbf41bdc56d78d219f7319
呂紹淳 <oslo0322@oslo-mba.local>
2015-09-20 13:31:49 +0800

    update

--- ToiletAlarm/ViewController.swift
+++ ToiletAlarm/ViewController.swift
@@ -12,16 +12,32 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var alarmLabel: UILabel!
     
+    @IBOutlet weak var alarmSetting: UISwitch!
+    
+    @IBAction func alarmTouch(sender: UISwitch) {
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.alarmSetting.canBecomeFirstResponder()
         // Do any additional setup after loading the view, typically from a nib.
-        ToiletLightHouseClient.sharedInstance.isToiletOccupied.subscribeNext{ nextStatus in
+            var temp = false
+            ToiletLightHouseClient.sharedInstance.isToiletOccupied.subscribeNext{ nextStatus in
             switch nextStatus {
             case OccupationStatus.Available:
-                self.alarmLabel.text = "Available"
-                self.alarmLabel.backgroundColor = UIColor.greenColor()
+                print(self.alarmSetting.state.rawValue)
+                if temp == true{
+                    self.alarmLabel.text = "快去!!!"
+                    self.alarmLabel.backgroundColor = UIColor.grayColor()
+                    temp = false
+                }else{
+                    self.alarmLabel.text = "Available"
+                    self.alarmLabel.backgroundColor = UIColor.greenColor()
+                }
+
             case OccupationStatus.Occupied:
                 self.alarmLabel.text = "Occupied"
+                temp = true
                 self.alarmLabel.backgroundColor = UIColor.redColor()
             case OccupationStatus.Unknown:
                 self.alarmLabel.text = "Unknown"
